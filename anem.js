8a0ab2c5de0fc3746dd7f8e83301c533100f154f
anemratnasari <118803025+anemratnasari@users.noreply.github.com>
2022-11-29 10:58:05 +0700

    Add files via upload

---
+++ anem.js
@@ -0,0 +1,9 @@
+// mencari luas lingkaran 
+var pangilanuntukinput = require('readline');
+var pi =pangilanuntukinput.questionlnt("masukan nilai pi:");
+var jarijari =pangilanuntukinput.questionlnt("masuksn nilsi jari-jari lingkaran:");
+if (pi = 22/7)
+  console.log("luas lingkaran tersebut adalah " +22/7*jarijari*jarijari);
+  else
+  if(pi = 3.14)
+  console.log("luas lingkaran tersebut adalah " + 3.14*jarijari*jarijari);
